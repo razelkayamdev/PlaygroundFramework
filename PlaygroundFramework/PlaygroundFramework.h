ef8758eec50d278947630698fc5b6f7d0859909e
Raz Elkayam <raz@appoxee.com>
2015-06-23 18:32:58 +0300

    Initial Commit

---
+++ PlaygroundFramework/PlaygroundFramework.h
@@ -0,0 +1,13 @@
+//
+//  PlaygroundFramework.h
+//  PlaygroundFramework
+//
+//  Created by Raz Elkayam on 6/23/15.
+//  Copyright (c) 2015 Raz Elkayam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlaygroundFramework : NSObject
+
+@end
---
+++ PlaygroundFramework/PlaygroundFramework.m
@@ -0,0 +1,13 @@
+//
+//  PlaygroundFramework.m
+//  PlaygroundFramework
+//
+//  Created by Raz Elkayam on 6/23/15.
+//  Copyright (c) 2015 Raz Elkayam. All rights reserved.
+//
+
+#import "PlaygroundFramework.h"
+
+@implementation PlaygroundFramework
+
+@end
